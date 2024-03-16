ca0adc3030a5fc80d997a244439153c890426d33
neoko <33611354+neokofg@users.noreply.github.com>
2024-03-16 21:40:51 +0700

    feat/loader

---
+++ src/components/loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return (
+        <div className={"w-full h-[100vh] flex items-center justify-center text-6xl"}>
+            LOADER
+        </div>
+    );
+}
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,15 +3,41 @@ import Customers from "../components/customers/customers.jsx";
 import Showcase from "../components/projects/showcase.jsx";
 import Services from "../components/services/services.jsx";
 import Footer from "../components/footer.jsx";
+import {useEffect, useState} from "react";
+import Loader from "../components/loader.jsx";
 
 export default function Index() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        // Функция, которая будет вызвана после загрузки страницы
+        const handleLoad = () => {
+            setIsLoading(false); // Скрываем лоадер
+            document.querySelector('.body-content').classList.add('loaded');
+        };
+
+        window.addEventListener('load', handleLoad);
+
+        // Если страница уже загружена до момента монтирования этого компонента, сразу скроем лоадер
+        if (document.readyState === 'complete') {
+            setIsLoading(false);
+        }
+
+        return () => {
+            window.removeEventListener('load', handleLoad);
+        };
+    }, []);
+
     return (
         <div>
-            <Welcome/>
-            <Customers/>
-            <Showcase/>
-            <Services/>
-            <Footer/>
+            {isLoading && <Loader />}
+            <div className={"body-content"}>
+                <Welcome/>
+                <Customers/>
+                <Showcase/>
+                <Services/>
+                <Footer/>
+            </div>
         </div>
     );
 }
